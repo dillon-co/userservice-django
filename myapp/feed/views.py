41c2282e448d88330392c42fe9f1b99beb319407
Dillon <dilloncortez@gmail.com>
2019-01-22 17:53:41 -0700

    finished adding API

--- myapp/feed/views.py
+++ myapp/feed/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 from .models import Demographics
+import json
 
 # Create your views here.
 
-
-
-
-def home(request):
+def demographics_context():
     if Demographics.objects.all().count() > 0:
         m_c, f_c, o_c = Demographics.objects.all().last().male_count, Demographics.objects.all().last().female_count, Demographics.objects.all().last().other_count
     else:
@@ -17,8 +16,16 @@ def home(request):
         'male_count':m_c,
         'female_count':f_c,
         'other_count':o_c,
-        'user_ids':User.objects.all().values_list('id', flat=True)
+        'user_ids':list(User.objects.all().values_list('id', flat=True))
     }
+    return context
+
+
+def home(request):
+    context = demographics_context()
     return render(request, 'feed/home.html', context)
 
 # def base(request)
+def view_demographics(request):
+    response = json.dumps([demographics_context()])
+    return HttpResponse(response, content_type='text/json')
