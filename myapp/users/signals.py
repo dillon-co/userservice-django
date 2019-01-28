20a004348336d47aec44faff5ebc30adb9713183
Dillon <dilloncortez@gmail.com>
2019-01-28 14:32:02 -0700

    updated dockerfile

--- myapp/users/signals.py
+++ myapp/users/signals.py
@@ -8,9 +8,10 @@ from feed.models import Demographics
 def current_demographics():
     if Demographics.objects.all().count() > 0:
         c_d = Demographics.objects.all().last()
-        return c_d.male_count, c_d.female_count, c_d.other_count
+        # return c_d
     else:
-        0, 0, 0
+        c_d = Demographics.objects.create(male_count:0, female_count:0, other_count:0)
+    return c_d
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
@@ -20,12 +21,15 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
-    male_count, female_count, other_count = current_demographics()
+    demographics = current_demographics()
     if instance.profile.gender == 'Male':
-        male_count += 1
+        male_count = demographics.male_count + 1
+        demographics.update(male_count: male_count)
     elif instance.profile.gender == 'Female':
-        female_count += 1
+        female_count = demographics.female_count + 1
+        demographics.update(female_count: female_count)
     elif instance.profile.gender == 'Other':
-        other_count += 1
+        otehr_count = demographics.otehr_count + 1
+        demographics.update(otehr_count: otehr_count)
 
-    Demographics.objects.create(male_count=male_count, female_count=female_count, other_count=other_count)
+    demographics.save()
